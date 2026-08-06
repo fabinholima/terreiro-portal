982388c490905794d647b53ab8f0db947fabe595
fabinholima <fabio.delima21@gmail.com>
2026-08-06 19:26:41 -0400

    feat: add gallery routes

--- start/routes.ts
+++ start/routes.ts
@@ -9,8 +9,11 @@ import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 
 const EventsController = () => import('#controllers/admin/events_controller')
+const AlbumsController = () => import('#controllers/admin/albums_controller')
+const GalleryController = () => import('#controllers/gallery_controller')
 
 router.on('/').render('pages/home').as('home')
+router.get('/galeria', [GalleryController, 'index']).as('gallery')
 
 router
   .group(() => {
@@ -32,5 +35,15 @@ router
     router.get('/admin/events/:id/edit', [EventsController, 'edit'])
     router.post('/admin/events/:id', [EventsController, 'update'])
     router.post('/admin/events/:id/delete', [EventsController, 'destroy'])
+
+    router.get('/admin/albums', [AlbumsController, 'index'])
+    router.get('/admin/albums/create', [AlbumsController, 'create'])
+    router.post('/admin/albums', [AlbumsController, 'store'])
+    router.get('/admin/albums/:id/edit', [AlbumsController, 'edit'])
+    router.post('/admin/albums/:id', [AlbumsController, 'update'])
+    router.post('/admin/albums/:id/photos', [AlbumsController, 'upload'])
+    router.post('/admin/albums/:id/photos/:photoId', [AlbumsController, 'updatePhoto'])
+    router.post('/admin/albums/:id/photos/:photoId/delete', [AlbumsController, 'deletePhoto'])
+    router.post('/admin/albums/:id/delete', [AlbumsController, 'destroy'])
   })
   .use(middleware.auth())
