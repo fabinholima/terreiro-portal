617d05cc98c915452913e4776b7e395b53407a04
fabinholima <fabio.delima21@gmail.com>
2026-08-07 14:45:24 -0400

    feat: atualiza schema de usuários com perfis

--- database/schema.ts
+++ database/schema.ts
@@ -8,18 +8,30 @@ import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 
 export class UserSchema extends BaseModel {
-  static $columns = ['createdAt', 'email', 'fullName', 'id', 'password', 'updatedAt'] as const
+  static $columns = ['createdAt', 'email', 'fullName', 'id', 'isActive', 'password', 'role', 'updatedAt'] as const
   $columns = UserSchema.$columns
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
+
   @column()
   declare email: string
+
   @column()
   declare fullName: string | null
+
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare isActive: boolean
+
   @column({ serializeAs: null })
   declare password: string
+
+  @column()
+  declare role: 'admin' | 'editor'
+
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
 }
