a9747559be0927e0e2d49f22c9ff55e4fe98a223
fabinholima <fabio.delima21@gmail.com>
2026-08-07 12:45:23 -0400

    feat(documents): add document model

---
+++ app/models/document.ts
@@ -0,0 +1,34 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Document extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare category: string | null
+
+  @column()
+  declare version: string | null
+
+  @column.date()
+  declare documentDate: DateTime | null
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare filePath: string
+
+  @column()
+  declare isPublic: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
