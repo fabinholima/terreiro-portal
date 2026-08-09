f7a22a83b510ccd2ed65f19e4e38deef8de07723
fabinholima <fabio.delima21@gmail.com>
2026-08-09 12:02:20 -0400

    style(cms): carrega estilos do submenu

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,68 +3,12 @@ import '../css/agenda.css'
 import '../css/social-contact.css'
 import '../css/documents.css'
 import '../css/gallery.css'
+import '../css/cms-navigation.css'
 import Alpine from 'alpinejs'
 
 Alpine.data('alert', function () {
-  return {
-    isVisible: false,
-    dismiss() {
-      this.isVisible = false
-    },
-    init() {
-      setTimeout(() => {
-        this.isVisible = true
-      }, 80)
-      setTimeout(() => {
-        this.dismiss()
-      }, 5000)
-    },
-  }
+  return { isVisible:false, dismiss(){this.isVisible=false}, init(){setTimeout(()=>{this.isVisible=true},80);setTimeout(()=>{this.dismiss()},5000)} }
 })
-
-Alpine.data('mobileMenu', () => ({
-  open: false,
-  toggle() {
-    this.open = !this.open
-  },
-  close() {
-    this.open = false
-  },
-}))
-
-Alpine.data('lightbox', () => ({
-  image: null,
-  images: [],
-  index: -1,
-  init() {
-    this.images = Array.from(this.$root.querySelectorAll('[data-lightbox-image]'))
-      .map((element) => element.getAttribute('data-lightbox-image'))
-      .filter(Boolean)
-  },
-  open(image) {
-    this.image = image
-    this.index = this.images.indexOf(image)
-    document.documentElement.style.overflow = 'hidden'
-  },
-  close() {
-    this.image = null
-    this.index = -1
-    document.documentElement.style.overflow = ''
-  },
-  next() {
-    if (!this.images.length) return
-    this.index = this.index < 0 ? 0 : (this.index + 1) % this.images.length
-    this.image = this.images[this.index]
-  },
-  previous() {
-    if (!this.images.length) return
-    this.index = this.index < 0 ? 0 : (this.index - 1 + this.images.length) % this.images.length
-    this.image = this.images[this.index]
-  },
-  get counter() {
-    if (!this.image || this.index < 0) return ''
-    return `${this.index + 1} / ${this.images.length}`
-  },
-}))
-
+Alpine.data('mobileMenu',()=>({open:false,toggle(){this.open=!this.open},close(){this.open=false}}))
+Alpine.data('lightbox',()=>({image:null,images:[],index:-1,init(){this.images=Array.from(this.$root.querySelectorAll('[data-lightbox-image]')).map((e)=>e.getAttribute('data-lightbox-image')).filter(Boolean)},open(image){this.image=image;this.index=this.images.indexOf(image);document.documentElement.style.overflow='hidden'},close(){this.image=null;this.index=-1;document.documentElement.style.overflow=''},next(){if(!this.images.length)return;this.index=this.index<0?0:(this.index+1)%this.images.length;this.image=this.images[this.index]},previous(){if(!this.images.length)return;this.index=this.index<0?0:(this.index-1+this.images.length)%this.images.length;this.image=this.images[this.index]},get counter(){return !this.image||this.index<0?'':`${this.index+1} / ${this.images.length}`}}))
 Alpine.start()
